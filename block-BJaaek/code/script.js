0181d6dd4f92bbca8ad7b7375cf16cdc144c91ac
Kushal Nandwani <kushalnandwani98@gmail.com>
2022-02-15 17:57:08 +0530

    v

---
+++ block-BJaaek/code/script.js
@@ -0,0 +1,18 @@
+let box1 = document.querySelector(".first");
+let box2 = document.querySelector(".second");
+
+box1.style.backgroundColor = "#6f3dfb";
+
+// box1.addEventListener("onclick", changeColor);
+
+
+// #6f3dfb
+// #ff34bf
+// #83f9d3
+// #eb1bc5
+// #bc47f4
+// #6ac701
+// #cc6abd
+// #5ec5b2
+// #c1acfe
+// #10b8f0
