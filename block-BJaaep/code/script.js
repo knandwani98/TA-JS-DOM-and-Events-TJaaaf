5b7391a15c612d42e03b76a13b35d5308cad286c
Kushal Nandwani <kushalnandwani98@gmail.com>
2022-02-17 23:57:40 +0530

    v

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,14 +1,5 @@
 let first = document.querySelectorAll(".first li");
-
-// first.forEach((box, index) => {
-//     box.addEventListener("click", (event) => {
-//         event.target.innerText = index + 1;
-
-//         setTimeout(() => {
-//             event.target.innerText = "";
-//         }, 5000)
-//     })
-// })
+let second = document.querySelector(".second");
 
 first.forEach((box, index) =>{
     box.addEventListener("click", (event) => {
@@ -19,3 +10,15 @@ first.forEach((box, index) =>{
         }, 5000)
     })
 })
+
+let secondBoxes = second.querySelectorAll("li");
+secondBoxes.forEach((box, index) => {
+    box.setAttribute("data-text", index + 1);
+})
+second.addEventListener("click", (event) => {
+    let text = event.target.dataset.text;
+    event.target.innerText = text;
+    setTimeout(() => {
+        event.target.innerText = "";
+    }, 5000)
+});
